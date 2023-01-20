43c2a436747ef4db30090ec6970ca83fe317bc74
Samiul Hoque <samiul1018@gmail.com>
2023-01-20 16:07:07 +0100

    Update final.cpp

--- final.cpp
+++ final.cpp
@@ -5,3 +5,14 @@ std::string concatenate(const Args&... strs) {
     (result.append(strs), ...);
     return result;
 }
+
+template <typename... Args>
+std::string concatenate(const Args&... strs) {
+    std::string result;
+    if constexpr(sizeof...(strs) != 0)
+    {
+        result.reserve((std::string_view(strs).size() + ...));
+        (result.append(strs), ...);
+    }
+    return result;
+}
